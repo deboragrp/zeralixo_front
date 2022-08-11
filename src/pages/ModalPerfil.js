dab43db80c83771bc6c78d6eae2321a85314df5a
deboragrp <debora.germana.319@gmail.com>
2022-08-11 20:33:15 -0300

    changes

--- src/pages/ModalPerfil.js
+++ src/pages/ModalPerfil.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
@@ -10,92 +9,63 @@ import Form from "react-bootstrap/Form";
 function ModalPerfil(props) {
   const handleClose = () => setShow(false);
   const [setShow] = useState(false);
-=======
-import React,  { useState }  from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Modal from 'react-bootstrap/Modal';
-import Row from 'react-bootstrap/Row';
-import Form from "react-bootstrap/Form";
-
-function ModalPerfil(props) {
-
-    const handleClose = () => setShow(false);
-    const [setShow] = useState(false);
->>>>>>> 3f48419afc52d38740bfb0206c772a3a3206b3e0
 
   return (
     <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
       <Modal.Header closeButton>
-<<<<<<< HEAD
         <Modal.Title id="contained-modal-title-vcenter">
           Editar Perfil
         </Modal.Title>
-=======
-        <Modal.Title id="contained-modal-title-vcenter">Editar Perfil</Modal.Title>
->>>>>>> 3f48419afc52d38740bfb0206c772a3a3206b3e0
       </Modal.Header>
       <Modal.Body className="show-grid">
         <Container>
           <Row>
-<<<<<<< HEAD
-            <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Label>Nome Completo</Form.Label>
-              <Form.Control type="text" placeholder="Nome completo" />
+            <Row>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Nome Completo</Form.Label>
+                <Form.Control type="text" placeholder="Nome completo" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>E-mail</Form.Label>
+                <Form.Control type="e-mail" placeholder="Enter e-mail" />
+              </Form.Group>
+            </Row>
+
+            <Row className="mb-3">
+              <Form.Group as={Col}>
+                <Form.Label>Senha</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="ID do seu condomínio"
+                />
+              </Form.Group>
+            </Row>
+
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label>Função</Form.Label>
+
+              <Form.Select defaultValue="Escolha...">
+                <option>Escolha...</option>
+                <option>Regar</option>
+                <option>Colher</option>
+                <option>Limpar</option>
+                <option>Plantar</option>
+              </Form.Select>
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>E-mail</Form.Label>
-              <Form.Control type="e-mail" placeholder="Enter e-mail" />
-            </Form.Group>
-          </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Seu andar</Form.Label>
+                <Form.Control type="number" placeholder="Escolha seu andar" />
+              </Form.Group>
 
-          <Row className="mb-3">
-            <Form.Group as={Col}>
-              <Form.Label>Senha</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="ID do seu condomínio"
-              />
-            </Form.Group>
-          </Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Nº do apt</Form.Label>
+                <Form.Control type="number" placeholder="Número do seu apt" />
+              </Form.Group>
+            </Row>
 
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>Função</Form.Label>
-=======
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Nome Completo</Form.Label>
-            <Form.Control type="text" placeholder="Nome completo" />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>E-mail</Form.Label>
-            <Form.Control type="e-mail" placeholder="Enter e-mail" />
-          </Form.Group>
-        </Row>
-
-        <Row className="mb-3">
-          <Form.Group as={Col}>
-            <Form.Label>Senha</Form.Label>
-            <Form.Control type="password" placeholder="ID do seu condomínio" />
-          </Form.Group>
-          </Row>
-
-          <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>Função</Form.Label>
->>>>>>> 3f48419afc52d38740bfb0206c772a3a3206b3e0
-            <Form.Select defaultValue="Escolha...">
-              <option>Escolha...</option>
-              <option>Regar</option>
-              <option>Colher</option>
-              <option>Limpar</option>
-              <option>Plantar</option>
-            </Form.Select>
-          </Form.Group>
-
-          <Row className="mb-3">
-<<<<<<< HEAD
             <Form.Group as={Col} controlId="formGridPassword">
               <Form.Label>Seu andar</Form.Label>
               <Form.Control type="number" placeholder="Escolha seu andar" />
@@ -106,48 +76,15 @@ function ModalPerfil(props) {
               <Form.Control type="number" placeholder="Número do seu apt" />
             </Form.Group>
           </Row>
-=======
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Seu andar</Form.Label>
-            <Form.Control type="number" placeholder="Escolha seu andar" />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Nº do apt</Form.Label>
-            <Form.Control type="number" placeholder="Número do seu apt" />
-          </Form.Group>
-          </Row>
-
->>>>>>> 3f48419afc52d38740bfb0206c772a3a3206b3e0
         </Container>
       </Modal.Body>
 
       <Modal.Footer>
-<<<<<<< HEAD
-        <Button variant="success" onClick={handleClose}>
-          Salvar
-        </Button>
-        <Button variant="secondary" onClick={handleClose}>
-          Fechar
-        </Button>
+        <Button variant="success" onClick={handleClose}></Button>
+        <Button variant="secondary" onClick={handleClose}></Button>
       </Modal.Footer>
     </Modal>
   );
 }
 
 export default ModalPerfil;
-=======
-          <Button variant="primary" onClick={handleClose}>
-           Salvar          
-          </Button>
-          <Button variant="secondary" onClick={handleClose}>
-            Fechar
-          </Button>
-        </Modal.Footer>
-    </Modal>
-    
-  );
-}
-
-export default ModalPerfil
->>>>>>> 3f48419afc52d38740bfb0206c772a3a3206b3e0
--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-=======
-import React,  { useState } from "react";
->>>>>>> 3f48419afc52d38740bfb0206c772a3a3206b3e0
 import Navbar2 from "../components/navbar2";
 import { Container } from "react-bootstrap";
+import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-<<<<<<< HEAD
+
 import Avatar from "../images/avatar.png";
 import CardGroup from "react-bootstrap/CardGroup";
 import ModalPerfil from "./ModalPerfil";
@@ -21,32 +17,15 @@ import ListGroup from "react-bootstrap/ListGroup";
 import Form from "react-bootstrap/Form";
 
 function Perfil() {
-=======
-import Avatar from "../images/avatar.png"
-import CardGroup from "react-bootstrap/CardGroup";
-import ModalPerfil from "./ModalPerfil";
-import tomate from "../images/tomate.jpg"
-import coentro from "../images/coentro.jpg"
-import cenoura from "../images/cenoura.png"
-import Badge from 'react-bootstrap/Badge';
-import ListGroup from 'react-bootstrap/ListGroup';
-
-
-
-function Perfil() {
-  const [task, setTask] = React.useState();
-  const [allTasks, setAllTasks] = React.useState([]);
->>>>>>> 3f48419afc52d38740bfb0206c772a3a3206b3e0
   const [modalShow, setModalShow] = useState(false);
 
   return (
     <div>
       <Navbar2 />
-
       {/* Card Perfil */}
+
       <Container className="py-5">
         <Row>
-<<<<<<< HEAD
           <Col className="col-12  col-lg-4 col-md-6">
             <Card
               className="shadow"
@@ -90,7 +69,6 @@ function Perfil() {
                 </>
               </Card.Body>
             </Card>
-            <br></br>
 
             {/* Card Status */}
             <Card
@@ -286,215 +264,6 @@ function Perfil() {
                   </Card>
                 </CardGroup>
               </Col>
-=======
-          <Col className="col-12  col-lg-4 ">
-          <Card style={{ width: '18rem',height:"18rem", background: ' #F5F3F3' }}>
-             <Card.Img
-                src={Avatar}
-                alt="avatar"
-                class="rounded-circle img-fluid"
-                style={{ width: "130px", marginLeft: "28%" }}
-              />      
-          <Card.Body>
-          <Card.Title 
-                style={{ marginLeft: "28%" }}>Maria da Silva</Card.Title>
-        <Card.Text  style={{ marginLeft: "35%" }}>
-                   <p>20 Pontos</p>
-        </Card.Text>
-                  <>
-                <Button variant="primary" 
-                        onClick={() => setModalShow(true)}                        
-                        class="btn btn-outline-success"
-                        style={{ marginLeft: "28%" }}
-                        >Editar Perfil</Button>
-
-                   <ModalPerfil show={modalShow} onHide={() => setModalShow(false)} />
-                  </>
-          </Card.Body>
-        </Card><br></br>
-
-                 {/* Card Status */}
-        <Card className="text-center"   style={{ width: "18rem"}}>
-                <Card.Header>Status</Card.Header>       
-            </Card>
-      <ListGroup as="ol" numbered style={{ width: "18rem"}}>
-      <ListGroup.Item
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-      >
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Tarefas</div>
-          Total de Tarefas         
-        </div>
-        <Badge bg="primary" pill>
-          14
-        </Badge>
-      </ListGroup.Item>
-
-      <ListGroup.Item
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-      >
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Abertas</div>
-          Tarefas em andamento     
-          </div>
-        <Badge bg="primary" pill>
-          3
-        </Badge>
-        </ListGroup.Item>
-
-      <ListGroup.Item
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-      >
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Finalizadas</div>
-            Total de tarefas finalizadas        
-          </div>
-        <Badge bg="primary" pill>
-          9
-        </Badge>
-      </ListGroup.Item>
-
-      <ListGroup.Item
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-      >
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">Canceladas</div>
-           Total de tarefas canceladas        
-          </div>
-        <Badge bg="primary" pill>
-          2
-        </Badge>
-      </ListGroup.Item>
-    </ListGroup>
-          </Col>
-
-  
-                  {/* Card Task */}
-          <Col className="col-lg-8"   style={{}}>
-            <Card className="text-center">
-                <Card.Header>Task</Card.Header>       
-            </Card>
-
-            <Card className="mb-4 "
-            style={{padding: "26px"}}>
-              <form class="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
-                <div class="col-12">
-                  <div class="form-outline">
-                    <input
-                      onChange={textOnTextArea}
-                      type="text"
-                      id="form1"
-                      class="form-control"
-                      placeholder="Insira uma task"
-                    />
-                  </div>
-                </div>
-
-                <div class="col-12">
-                  <button
-                    onClick={clickbutton}
-                    type="submit"
-                    class="btn btn-primary fw-bold"
-                  >Salvar
-                  </button>
-                </div>
-              </form>
-
-              <table class="table mb-4 ">
-                <thead>
-                  <tr>
-                    <th scope="col">Nº</th>
-                    <th scope="col">Para fazer</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Ações</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>{allTasks}</td>
-                    <td>Em progresso</td>
-                    <td>
-                      <button type="submit" class="btn btn-danger">
-                        Delete
-                      </button>
-
-                      <button type="submit" class="btn btn-success ms-1">
-                        Finalizado
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </Card>
-
-
-                 
-                 {/* Card troca de pontos */}
-            <Row>
-            <Col className="col-lg-12">
-                   
-            <Card className="text-center">
-                <Card.Header>Troca de Pontos</Card.Header>       
-            </Card>
-
-             <CardGroup>
-             <Card>
-               <Card.Img variant="top" src={tomate} style={{  width: "150px",  marginLeft: "18%", marginTop:"10%"}}/>
-            <Card.Body>
-             <Card.Title style={{ marginLeft: "30%"}}>Tomates</Card.Title>
-             <Card.Text>
-             O tomate é uma boa fonte de vitaminas A, C e do complexo B. 
-             O mesmo também possui quantidades significativas de sais minerais, 
-             como Ferro, Potássio, Cálcio, Magnésio, além de conter 
-             pouquíssimas calorias.
-             </Card.Text>
-                      <button type="submit" class="btn btn-success" style={{ marginLeft: "32%"}} >
-                        Trocar
-                      </button>
-            </Card.Body>
-             </Card>
-
-             <Card>
-               <Card.Img variant="top" src={coentro} style={{   width: "140px",  marginLeft: "18%", marginTop:"4%"}}/>
-             <Card.Body>
-             <Card.Title style={{ marginLeft: "30%"}}>Coentro</Card.Title>
-             <Card.Text>
-             O coentro contém propriedades digestivas e anti-inflamatórias, 
-             ajudando a combater a má digestão e a constipação. Tambem diminui a pressão arterial 
-             e auxiliar no tratamento da diabetes.
-             </Card.Text><br></br>
-                      <button type="submit" class="btn btn-success " style={{ marginLeft: "32%"}}>
-                        Trocar
-                      </button>
-             </Card.Body>
-            </Card>
-         
-             <Card>
-                <Card.Img variant="top" src={cenoura} style={{  width: "140px",  marginLeft: "18%", marginTop:"4%"}} />
-         <Card.Body>
-          <Card.Title style={{ marginLeft: "30%"}}>Cenoura</Card.Title>
-          <Card.Text>
-          A cenoura é rica em sais minerais, como ferro e cálcio, 
-          vitaminas K, A, C, E e do complexo B.
-          Por isso ela ajuda na saúde dos olhos, pele e até do cérebro, 
-          além de ter influência positiva no colesterol.
-          </Card.Text>
-                      <button type="submit" class="btn btn-success" style={{ marginLeft: "32%"}}>
-                        Trocar
-                      </button>
-        </Card.Body>
-     
-               </Card>
-             </CardGroup>
-            </Col>
-
->>>>>>> 3f48419afc52d38740bfb0206c772a3a3206b3e0
             </Row>
           </Col>
         </Row>
